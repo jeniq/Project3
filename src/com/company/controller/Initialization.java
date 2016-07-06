f06227d6ec7342fed7271a89c67978930befba76
Jenia <gr.yevhen@gmail.com>
2016-07-06 12:01:44 +0300

    Initialization data

---
+++ src/com/company/controller/Initialization.java
@@ -0,0 +1,9 @@
+package com.company.controller;
+
+/**
+ * This interface contains paths to documents.
+ */
+public interface Initialization {
+    String XML_PATH = "C:\\Users\\Женя\\IdeaProjects\\Project3\\src\\com\\company\\xml\\Weapon.xml";
+    String XSD_PATH = "C:\\Users\\Женя\\IdeaProjects\\Project3\\src\\com\\company\\xml\\Weapon.xsd";
+}
